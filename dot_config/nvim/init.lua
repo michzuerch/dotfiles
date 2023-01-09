ebd696a7da5eecfd94a2f387d71ba266f095f984
Michael Zuercher <michzuerch@gmail.com>
2023-01-09 13:10:01 +0100

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,14 @@
-require("lazyvim.config.lazy")
+-- References to ./lua/
+
+-- Load global functions
+require("core.globals")
+-- Plugin management via lazy
+require("core.lazy")
+-- Plugin management via Packer
+-- require("core.packer")
+-- "Global" Keymappings
+require("core.mappings")
+-- All non plugin related (vim) options
+require("core.options")
+-- Vim autocommands/autogroups
+require("core.autocmd")
